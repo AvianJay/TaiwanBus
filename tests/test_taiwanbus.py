b62c454020f02f898d7b2dd7c14e3a6ea0e1fd5e
AvianJay <jay20090728@gmail.com>
2024-11-04 07:16:05 +0800

    fix

--- tests/test_taiwanbus.py
+++ tests/test_taiwanbus.py
@@ -6,7 +6,7 @@ import taiwanbus
 class TestTaiwanBus(unittest.TestCase):
 
     def test_taiwanbus(self):
-        taiwanbus.updatedb()
+        taiwanbus.update_database()
         data = asyncio.run(taiwanbus.fetch_route(304030))
         self.assertIsInstance(data, list, "fetch_route() should return a list")
 
