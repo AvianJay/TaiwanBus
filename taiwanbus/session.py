65a59913fea631b64bad8fe9eb6ecd71264db3c2
AvianJay <jay20090728@gmail.com>
2025-08-20 18:04:04 +0800

    add bus session

---
+++ taiwanbus/session.py
@@ -0,0 +1,26 @@
+# My code is shit.
+# Bus session
+from . import api
+from datetime import datetime
+
+
+class BusSession:
+    def __init__(self, route_key, provider=api.Provider.TWN):
+        super()
+        self.BUSINFO = None
+        self.LAST_UPDATE = 0
+        self.ROUTE_KEY = route_key
+        api.update_provider(provider)
+
+    def update(self):
+        self.BUSINFO = api.get_complete_bus_info(self.ROUTE_KEY)
+        self.LAST_UPDATE = datetime.now().timestamp()
+
+    def get_stop(self, stopid):
+        for path in self.BUSINFO.values():
+            for stop in path["stops"]:
+                if stop.get("stop_id") == stopid:
+                    return stop
+
+    def get_path(self, pathid):
+        return self.get(pathid)
