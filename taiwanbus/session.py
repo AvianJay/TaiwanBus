ec976671db3805072aae7843a03902311c044530
AvianJay <jay20090728@gmail.com>
2025-08-24 11:03:20 +0800

    add simulate

--- taiwanbus/session.py
+++ taiwanbus/session.py
@@ -1,15 +1,24 @@
 # My code is shit.
 # Bus session
+import time
+import threading
 from . import api
 from datetime import datetime
 
 
 class BusSession:
-    def __init__(self, route_key, provider=api.Provider.TWN):
+    def __init__(self, route_key, provider=api.Provider.TWN, simulate_rate=1):
         super()
         self.BUSINFO = None
+        self.SIMULATED_BUSINFO = None
         self.LAST_UPDATE = 0
         self.ROUTE_KEY = route_key
+        self.SIMULATE_THREAD = threading.Thread(
+            target=self.simulate_runner,
+            daemon=True,
+        )
+        self.SIMULATE_STOPED = True
+        self.SIMULATE_RATE = simulate_rate
         api.update_provider(provider)
 
     def update(self):
@@ -24,3 +33,28 @@ class BusSession:
 
     def get_path(self, pathid):
         return self.BUSINFO.get(pathid)
+
+    def simulate_runner(self):
+        while True:
+            if self.SIMULATE_STOPED:
+                break
+            now = datetime.now().timestamp()
+            time_diff = int(now - self.LAST_UPDATE)
+            self.SIMULATED_BUSINFO = self.BUSINFO.copy()
+            for path in self.SIMULATED_BUSINFO.values():
+                for stop in path["stops"]:
+                    stop["sec"] += time_diff
+            time.sleep(self.SIMULATE_RATE)
+
+    def start_simulate(self):
+        self.SIMULATE_STOPED = False
+        self.SIMULATE_THREAD.start()
+
+    def stop_simulate(self):
+        self.SIMULATE_STOPED = True
+
+    def get_simulated_info(self):
+        return self.SIMULATED_BUSINFO
+
+    def get_info(self):
+        return self.BUSINFO
